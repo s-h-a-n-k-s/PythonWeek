427a284615d791724046a5b0fdaf28c079208c67
s-h-a-n-k-s <ramsdentom@gmail.com>
2019-05-24 10:15:58 +0100

    added three new python files

---
+++ results.py
@@ -0,0 +1,7 @@
+physics = 80
+chemistry = 0
+math = 0
+total = physics + chemistry + math
+
+print("The result is:")
+print(total)
---
+++ results2.py
@@ -0,0 +1,13 @@
+physics = 80
+chemistry = 100
+math = 100
+total = physics + chemistry + math
+per = total * 100 / 450
+
+print("pyhsics:", physics)
+print("chemistry:", chemistry)
+print("maths:", math)
+print("-------------------------")
+print("total marks:", total)
+print("percentage:", per)
+print("-------------------------")
